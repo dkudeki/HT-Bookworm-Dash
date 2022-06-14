666ed3384384d366612e33e3954820191de12b6b
Deren Kudeki <dkudeki@illinois.edu>
2022-06-14 18:20:12 -0500

    Added more logging to pinpoint where things are breaking

--- bar_chart.py
+++ bar_chart.py
@@ -109,6 +109,8 @@ def update_figure(group, trim_at, drop_radio, counttype):
     logging.debug(results)
 
     df = results.frame(index=False, drop_unknowns=(drop_radio=='drop'))
+    logging.debug("Created DataFrame of results:")
+    loggind.debug(df)
     df = map_to_human_readable(df,group)
     logging.debug("Ran map to human readable")
     df = df.copy()
