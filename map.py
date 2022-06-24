4b1e590884d9a908eb50b1e6613745785d354524
Deren Kudeki <dkudeki@illinois.edu>
2022-06-24 18:46:23 -0500

    Don't import Event

--- map.py
+++ map.py
@@ -2,7 +2,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import  State, Event, Input, Output
+from dash.dependencies import  State, Input, Output
 import plotly
 import plotly.graph_objs as go
 import pandas as pd
