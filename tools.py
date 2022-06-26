c25edf5404600f7afb535fed2d8f50576b004afa
Deren Kudeki <dkudeki@illinois.edu>
2022-06-26 16:22:56 -0500

    import html and dcc from dash instead of the other libraries that have been combined into dash

--- tools.py
+++ tools.py
@@ -1,6 +1,5 @@
 from common import app
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html, dcc
 import plotly.graph_objs as go
 import logging
 import json
