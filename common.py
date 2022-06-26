c4bbb32c81017b7f79432ab6c96836564a01d7ab
Deren Kudeki <dkudeki@illinois.edu>
2022-06-26 17:38:42 -0500

    Try applying stylesheet

--- common.py
+++ common.py
@@ -3,9 +3,10 @@
 A common server than can be imported, rather than indivudally initialized.
 '''
 import dash
+import dash_bootstrap_components as dbc
 import bwypy
 
-app = dash.Dash(url_base_pathname='/app/',suppress_callback_exceptions=True)
+app = dash.Dash(url_base_pathname='/app/',suppress_callback_exceptions=True,external_stylesheets=[dbc.themes.COSMO])
 
 app.css.append_css({
     "external_url" : "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
