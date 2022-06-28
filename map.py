7da8ec32e48dc88cc2486e411dcaae43b2784902
Deren Kudeki <dkudeki@illinois.edu>
2022-06-27 21:30:32 -0500

    uncommented the logging statements

--- map.py
+++ map.py
@@ -258,19 +258,19 @@ def display_click_data(clickData, word, compare_word, mapscope):
             raise
     return html.Ul(links)
 
-#@app.callback(
+@app.callback(
 #    Output('word_search_button','disabled'),
-#    Input('word_search_button', 'n_clicks'),
-#    Input('main-map-graph', 'figure')
-#)
-#def update_button(n_clicks,figure):
-#    context = dash.callback_context.triggered[0]['prop_id'].split('.')[0]
-#    context_value = dash.callback_context.triggered[0]['value']
-#    logging.debug(context)
-#    logging.debug(context_value)
-#    logging.debug(n_clicks)
-#    logging.debug(word_query)
-#    logging.debug(dash.callback_context.triggered)
+    Input('word_search_button', 'n_clicks'),
+    Input('main-map-graph', 'figure')
+)
+def update_button(n_clicks,figure):
+    context = dash.callback_context.triggered[0]['prop_id'].split('.')[0]
+    context_value = dash.callback_context.triggered[0]['value']
+    logging.debug(context)
+    logging.debug(context_value)
+    logging.debug(n_clicks)
+    logging.debug(word_query)
+    logging.debug(dash.callback_context.triggered)
 #    if context == 'map-search-term-hidden' or (context == 'word_search_button' and context_value == None):
 #        logging.debug("Enable button")
 #    else:
