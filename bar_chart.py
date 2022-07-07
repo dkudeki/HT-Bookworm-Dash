c5cb21f8851b99322480a7aa80f9bb4fb6b4df6e
Deren Kudeki <dkudeki@illinois.edu>
2022-07-07 15:38:48 -0500

    Don't start out disabled

--- bar_chart.py
+++ bar_chart.py
@@ -66,7 +66,7 @@ Select a field and see the raw counts in the Bookworm database
 controls = html.Div([
         dcc.Markdown(header),
         html.Label("Facet Group", className='mb-2'),
-        dcc.Dropdown(id='bar-group-dropdown', options=facet_opts, value='languages', disabled=True),
+        dcc.Dropdown(id='bar-group-dropdown', options=facet_opts, value='languages', disabled=False),
         html.Label("Number of results to show", className='mb-2'),
         dcc.Slider(id='trim-slider', min=10, max=60, value=20, step=5,
                    marks={str(n): str(n) for n in range(10, 61, 10)}, className='py-0 px-0'),
