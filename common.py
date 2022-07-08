86605720184c69e2564adb102d54894c475f0222
Deren Kudeki <dkudeki@illinois.edu>
2022-07-08 12:30:34 -0500

    Try updating to handle google analytics

--- common.py
+++ common.py
@@ -6,7 +6,9 @@ import dash
 import dash_bootstrap_components as dbc
 import bwypy
 
-app = dash.Dash(url_base_pathname='/app/',suppress_callback_exceptions=True,external_stylesheets=[dbc.themes.BOOTSTRAP],show_undo_redo=True)
+external_scripts = [ 'https://www.googletagmanager.com/gtag/js?id=G-Y6QBMH29BH' ]
+
+app = dash.Dash(__name__,url_base_pathname='/app/',suppress_callback_exceptions=True,external_stylesheets=[dbc.themes.BOOTSTRAP],external_scripts=external_scripts,show_undo_redo=True)
 
 app.css.append_css({
     "external_url" : "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
